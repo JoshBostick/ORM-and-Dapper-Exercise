c93cf363fdf990a7107cfc40a9cc5334e8d387cb
Josh Bostick <jbostick99@gmail.com>
2024-01-31 09:09:47 -0600

    in progress

---
+++ ORM-Dapper/DepartmentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM departments");
+        }
+    }
+
+   
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var connString = config.GetConnectionString("DefaultConnection");
+            
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DepartmentRepository(conn);
+            var departments = repo.GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+            }
         }
     }
 }
